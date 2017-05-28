8760a653031edbf5b3a9c042be72902160eb0675
ptitle <pascaltitle@gmail.com>
2017-05-28 18:42:07 -0400

    miscellaneous small updates, and fix for CRAN note about registering native routines

--- rangeBuilder/R/RcppExports.R
+++ rangeBuilder/R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 shortDistInd <- function(mat1, mat2) {
-    .Call('rangeBuilder_shortDistInd', PACKAGE = 'rangeBuilder', mat1, mat2)
+    .Call(rangeBuilder_shortDistInd, mat1, mat2)
 }
 
